7df4e247625f7cf25ed4855cd3fcdbabf0af70e1
Standa Chvila <schvila@gmail.com>
2022-06-04 10:01:54 +0200

    express and simple modleware

---
+++ JSexpress/app.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+//add midleware volano pri kazdem req.
+app.use((req, res, next) => {
+  console.log('In the midleware.');
+  next(); // jinak se nevola dalsi midleware
+})
+
+app.use((req, res, next) => {
+  console.log('In the second midleware.');
+  res.send('<h1> Hello express</h1>');
+})
+
+const server = http.createServer(app);
+
+server.listen(3000);
